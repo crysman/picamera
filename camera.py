f274d9607ea19f4edf4357cc5ee84fb8f0bf8087
Martin crysman Zahradník <crysman@seznam.cz>
2018-12-10 22:37:47 +0100

    version 1.0

---
+++ camera.py
@@ -0,0 +1,42 @@
+# a python script for a timelapse video - capturing JPEG stills every N sec
+# (using Raspberry Pi camera module) https://projects.raspberrypi.org/en/projects/getting-started-with-picamera
+# to start automatically every (re)boot, make a symlink into /etc/systemd:
+#   sudo ln -s ~/camera.service /etc/systemd/system/camera.service
+# crysman (copyleft) 2018-10
+
+import picamera
+import time
+import os
+
+delay = 8
+
+
+if not os.path.exists("camimages"):
+    os.makedirs("camimages")
+
+with picamera.PiCamera() as camera:
+
+    #https://picamera.readthedocs.io/en/release-1.9/recipes1.html#capturing-consistent-images
+    # Wait for analog gain to settle on a higher value than 1
+    if False:
+        while camera.analog_gain <= 1:
+            time.sleep(0.1)
+        # Now fix the values
+        camera.shutter_speed = camera.exposure_speed
+        camera.exposure_mode = 'off'
+        g = camera.awb_gains
+        camera.awb_mode = 'off'
+        camera.awb_gains = g
+
+    #custom parameters:
+    camera.rotation = 180
+    camera.sharpness = 100
+    camera.resolution = (2592, 1944) #higher res not available :/
+    #camera.resolution = (3280, 2464) #(cam v2 ONLY!)
+
+    #let's capture continuously:
+    camera.start_preview()
+    time.sleep(2)
+    for filename in camera.capture_continuous('camimages/{timestamp:%Y%m%d%H%M%S}.jpg',format='jpeg',quality=93):
+        print("%s captured, sleeping %i sec.." % (filename,delay))
+        time.sleep(delay)
